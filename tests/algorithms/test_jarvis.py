ec358dbe0f429ab11c3b7451bfdb025c7b6a1b49
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2024-05-25 17:06:29 +0300

    tests for Jarvis' algorithm now passing

--- tests/algorithms/test_jarvis.py
+++ tests/algorithms/test_jarvis.py
@@ -4,24 +4,24 @@ from algogears.jarvis import jarvis
 
 def test_jarvis1():
     pts = [
-        Point(1, 4),
-        Point(0, 0),
-        Point(3, 3),
-        Point(3, 1),
-        Point(7, 0),
-        Point(5, 5),
-        Point(5, 2),
-        Point(9, 6),
+        Point.new(1, 4),
+        Point.new(0, 0),
+        Point.new(3, 3),
+        Point.new(3, 1),
+        Point.new(7, 0),
+        Point.new(5, 5),
+        Point.new(5, 2),
+        Point.new(9, 6),
     ]
-    hull = [Point(0, 0), Point(1, 4), Point(9, 6), Point(7, 0)]
+    hull = [Point.new(0, 0), Point.new(1, 4), Point.new(9, 6), Point.new(7, 0)]
 
     ans = jarvis(pts)
     assert ans == hull
 
 
 def test_jarvis2():
-    pts = [Point(3, 3), Point(1, 1), Point(5, 0)]
-    hull = [Point(1, 1), Point(3, 3), Point(5, 0)]
+    pts = [Point.new(3, 3), Point.new(1, 1), Point.new(5, 0)]
+    hull = [Point.new(1, 1), Point.new(3, 3), Point.new(5, 0)]
 
     ans = jarvis(pts)
     assert ans == hull
