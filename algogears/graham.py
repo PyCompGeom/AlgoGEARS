e1c3c4a739532e5d8a4fc0b5a06d200b4c3c9666
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2024-05-25 16:51:41 +0300

    add serialization to Graham's algorithm, respective tests now working

--- algogears/graham.py
+++ algogears/graham.py
@@ -1,42 +1,38 @@
 from math import pi
-from .core import PyCGAObject, Point
+from typing import Iterable
+from pydantic import BaseModel, Field
+from .core import Point
 
 
-class GrahamStepsTableRow(PyCGAObject):
-    def __init__(self, point_triple, is_angle_less_than_pi):
-        self.point_triple = point_triple
-        self.is_angle_less_than_pi = is_angle_less_than_pi
-
+class GrahamStepsTableRow(BaseModel):
+    point_triple: tuple[Point, Point, Point]
+    is_angle_less_than_pi: bool
+    
     def __eq__(self, other):
         return (
             isinstance(other, self.__class__) and
             self.point_triple == other.point_triple and
             self.is_angle_less_than_pi == other.is_angle_less_than_pi
         )
-    
-    def __str__(self):
+
+    def __str__(self) -> str:
         return f"[{str(self.point_triple)}, {str(self.is_angle_less_than_pi)}]"
-    
-    def __repr__(self):
-        return str(self)
 
 
-class GrahamStepsTable(PyCGAObject):
-    def __init__(self, ordered_points, rows=None):
-        super().__init__()
-        self.ordered_points = ordered_points
-        self.rows = rows if rows else []
+class GrahamStepsTable(BaseModel):
+    ordered_points: list[Point]
+    rows: list[GrahamStepsTableRow] = Field(default_factory=list)
     
-    def append(self, item):
+    def append(self, item: GrahamStepsTableRow) -> None:
         self.rows.append(item)
     
-    def extend(self, iterable):
+    def extend(self, iterable: Iterable[GrahamStepsTableRow]) -> None:
         self.rows.extend(iterable)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: int) -> GrahamStepsTableRow:
         return self.rows[key]
     
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: int, value: GrahamStepsTableRow) -> None:
         self.rows[key] = value
 
     def __eq__(self, other):
@@ -48,12 +44,9 @@ class GrahamStepsTable(PyCGAObject):
 
     def __str__(self):
         return f"[{', '.join(str(row) for row in self)}]"
-    
-    def __repr__(self):
-        return str(self)
 
 
-def graham(points):
+def graham(points: Iterable[Point]):
     if len(points) < 3:
         yield sorted(points, key=lambda p: (p.y, -p.x))
     else:
@@ -65,17 +58,18 @@ def graham(points):
         yield centroid
 
         origin = min(points, key=lambda p: (p.y, -p.x))
-        ordered_points = sort_points(points, centroid, origin)
+        ordered_points = sorted_points(points, centroid, origin)
         yield ordered_points
         yield origin
 
         ordered_points.append(origin)
-        steps_table = GrahamStepsTable(ordered_points)
+        steps_table = GrahamStepsTable(ordered_points=ordered_points)
         hull = make_hull(steps_table, ordered_points)
         ordered_points.pop()
+        steps_table.ordered_points.pop()
         
-        yield [row.point_triple for row in steps_table]
-        yield [row.is_angle_less_than_pi for row in steps_table]
+        yield [row.point_triple for row in steps_table.rows]
+        yield [row.is_angle_less_than_pi for row in steps_table.rows]
         yield steps_table
         yield steps_table
         yield steps_table
@@ -83,7 +77,7 @@ def graham(points):
         yield hull
 
 
-def sort_points(points, centroid, origin):
+def sorted_points(points: list[Point], centroid: Point, origin: Point) -> list[Point]:
     min_angle = Point.polar_angle(origin, centroid)
 
     def angle_and_dist(p):
@@ -94,16 +88,16 @@ def sort_points(points, centroid, origin):
     return sorted(points, key=angle_and_dist)
 
 
-def make_hull(steps_table, ordered_points):
+def make_hull(steps_table: GrahamStepsTable, ordered_points: list[Point]) -> list[Point]:
     res = ordered_points[:2]
 
     for point in ordered_points[2:]:
         while len(res) > 1 and Point.direction(res[-2], res[-1], point) >= 0:
-            steps_table.append(GrahamStepsTableRow((res[-2], res[-1], point), False))
+            steps_table.append(GrahamStepsTableRow(point_triple=(res[-2], res[-1], point), is_angle_less_than_pi=False))
             res.pop()
 
         if len(res) > 1:
-            steps_table.append(GrahamStepsTableRow((res[-2], res[-1], point), True))
+            steps_table.append(GrahamStepsTableRow(point_triple=(res[-2], res[-1], point), is_angle_less_than_pi=True))
         
         res.append(point)
 
--- tests/algorithms/test_graham.py
+++ tests/algorithms/test_graham.py
@@ -4,10 +4,10 @@ from algogears.graham import graham, GrahamStepsTable, GrahamStepsTableRow
 
 
 def test_graham1():
-    pts = [Point(7, 0), Point(3, 3), Point(0, 0)]
-    centroid = Point(3.3333333333333335, 1.0)
-    ordered = [Point(7, 0), Point(3, 3), Point(0, 0)]
-    origin = Point(7, 0)
+    pts = [Point.new(7, 0), Point.new(3, 3), Point.new(0, 0)]
+    centroid = Point.new(3.3333333333333335, 1.0)
+    ordered = [Point.new(7, 0), Point.new(3, 3), Point.new(0, 0)]
+    origin = Point.new(7, 0)
 
     triples = [
         (ordered[0], ordered[1], ordered[2]),
@@ -15,13 +15,13 @@ def test_graham1():
     ]
     are_angles_less_than_pi = [True, True]
 
-    steps_table = GrahamStepsTable(ordered)
+    steps_table = GrahamStepsTable(ordered_points=ordered)
     steps_table.extend(
-        GrahamStepsTableRow(triple, is_angle_lt_pi)
+        GrahamStepsTableRow(point_triple=triple, is_angle_less_than_pi=is_angle_lt_pi)
         for triple, is_angle_lt_pi in zip(triples, are_angles_less_than_pi)
     )
     
-    hull = [Point(7, 0), Point(3, 3), Point(0, 0)]
+    hull = [Point.new(7, 0), Point.new(3, 3), Point.new(0, 0)]
     
     ans = graham(pts)
     assert next(ans) == centroid
@@ -40,33 +40,33 @@ def test_graham1():
 
 def test_graham2():
     pts = [
-        Point(3, 10),
-        Point(6, 8),
-        Point(3, 5),
-        Point(2, 8),
-        Point(4, 8),
-        Point(5, 5),
-        Point(3, 3),
-        Point(7, 7),
-        Point(5, 0),
-        Point(0, 0),
-        Point(10, 3),
+        Point.new(3, 10),
+        Point.new(6, 8),
+        Point.new(3, 5),
+        Point.new(2, 8),
+        Point.new(4, 8),
+        Point.new(5, 5),
+        Point.new(3, 3),
+        Point.new(7, 7),
+        Point.new(5, 0),
+        Point.new(0, 0),
+        Point.new(10, 3),
     ]
-    centroid = Point(4.0, 7.666666666666667)
+    centroid = Point.new(4.0, 7.666666666666667)
     ordered = [
-        Point(5, 0),
-        Point(5, 5),
-        Point(10, 3),
-        Point(7, 7),
-        Point(6, 8),
-        Point(4, 8),
-        Point(3, 10),
-        Point(2, 8),
-        Point(0, 0),
-        Point(3, 5),
-        Point(3, 3),
+        Point.new(5, 0),
+        Point.new(5, 5),
+        Point.new(10, 3),
+        Point.new(7, 7),
+        Point.new(6, 8),
+        Point.new(4, 8),
+        Point.new(3, 10),
+        Point.new(2, 8),
+        Point.new(0, 0),
+        Point.new(3, 5),
+        Point.new(3, 3),
     ]
-    origin = Point(5, 0)
+    origin = Point.new(5, 0)
     triples = [
         (ordered[0], ordered[1], ordered[2]),
         (ordered[0], ordered[2], ordered[3]),
@@ -84,20 +84,20 @@ def test_graham2():
     ]
     are_angles_less_than_pi = [False, True, True, True, False, True, True, True, True, False, True, False, True]
     
-    steps_table = GrahamStepsTable(ordered)
+    steps_table = GrahamStepsTable(ordered_points=ordered)
     steps_table.extend(
-        GrahamStepsTableRow(triple, is_angle_lt_pi)
+        GrahamStepsTableRow(point_triple=triple, is_angle_less_than_pi=is_angle_lt_pi)
         for triple, is_angle_lt_pi in zip(triples, are_angles_less_than_pi)
     )
     
     hull = [
-        Point(5, 0),
-        Point(10, 3),
-        Point(7, 7),
-        Point(6, 8),
-        Point(3, 10),
-        Point(2, 8),
-        Point(0, 0)
+        Point.new(5, 0),
+        Point.new(10, 3),
+        Point.new(7, 7),
+        Point.new(6, 8),
+        Point.new(3, 10),
+        Point.new(2, 8),
+        Point.new(0, 0)
     ]
     
     ans = graham(pts)
@@ -117,31 +117,31 @@ def test_graham2():
 
 def test_graham3():
     pts = [
-        Point(2, 8),
-        Point(5, 6),
-        Point(7, 8),
-        Point(8, 11),
-        Point(7, 5),
-        Point(10, 7),
-        Point(11, 5),
-        Point(8, 2),
-        Point(1, 3),
-        Point(5, 2),
+        Point.new(2, 8),
+        Point.new(5, 6),
+        Point.new(7, 8),
+        Point.new(8, 11),
+        Point.new(7, 5),
+        Point.new(10, 7),
+        Point.new(11, 5),
+        Point.new(8, 2),
+        Point.new(1, 3),
+        Point.new(5, 2),
     ]
-    centroid = Point(4.666666666666667, 7.333333333333333)
+    centroid = Point.new(4.666666666666667, 7.333333333333333)
     ordered = [
-        Point(8, 2),
-        Point(7, 5),
-        Point(11, 5),
-        Point(10, 7),
-        Point(7, 8),
-        Point(8, 11),
-        Point(2, 8),
-        Point(1, 3),
-        Point(5, 2),
-        Point(5, 6)
+        Point.new(8, 2),
+        Point.new(7, 5),
+        Point.new(11, 5),
+        Point.new(10, 7),
+        Point.new(7, 8),
+        Point.new(8, 11),
+        Point.new(2, 8),
+        Point.new(1, 3),
+        Point.new(5, 2),
+        Point.new(5, 6)
     ]
-    origin = Point(8, 2)
+    origin = Point.new(8, 2)
     triples = [
         (ordered[0], ordered[1], ordered[2]),
         (ordered[0], ordered[2], ordered[3]),
@@ -158,19 +158,19 @@ def test_graham3():
     ]
     are_angles_less_than_pi = [False, True, True, False, False, True, True, True, True, True, False, True]
     
-    steps_table = GrahamStepsTable(ordered)
+    steps_table = GrahamStepsTable(ordered_points=ordered)
     steps_table.extend(
-        GrahamStepsTableRow(triple, is_angle_lt_pi)
+        GrahamStepsTableRow(point_triple=triple, is_angle_less_than_pi=is_angle_lt_pi)
         for triple, is_angle_lt_pi in zip(triples, are_angles_less_than_pi)
     )
 
     hull = [
-        Point(8, 2),
-        Point(11, 5),
-        Point(8, 11),
-        Point(2, 8),
-        Point(1, 3),
-        Point(5, 2)
+        Point.new(8, 2),
+        Point.new(11, 5),
+        Point.new(8, 11),
+        Point.new(2, 8),
+        Point.new(1, 3),
+        Point.new(5, 2)
     ]
     
     ans = graham(pts)
